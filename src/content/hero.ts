9b0348dc9c62fa47c697ad3758bcd1fc47c49d7e
Krzysztof Worwa <worwak1998@gmail.com>
2026-01-23 19:57:09 +0100

    feat(hero): add typewriter subtitle animation

--- src/content/hero.ts
+++ src/content/hero.ts
@@ -1,5 +1,9 @@
 export const HERO = {
     id: "#home",
     h1: "Zamień swoją ofertę w wirtualne doświadczenie",
-    h2: "Nowoczesne strony internetowe"
+      h2: [
+    "Nowoczesne strony internetowe",
+    "Wizualizacje 3D",
+    "Wirtualne spacery 360°",
+  ],
 }
