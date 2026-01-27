8f8720f42ae57dd18201baf17d9b03b8aacc0cba
Krzysztof Worwa <worwak1998@gmail.com>
2026-01-27 16:52:34 +0100

    feat(about): add content and About component

---
+++ src/content/about.ts
@@ -0,0 +1,11 @@
+export const ABOUT = {
+    id: "about",
+    header: "Zaufany Partner w rozwoju Twojej marki",
+    subHeader: "O nas",
+    description: "Specjalizujemy się w cyfrowych rozwiązaniach, które przyciągają uwagę klientów i budują zaufanie do Twojej marki. Działamy kompleksowo - od pomysłu po gotowy projekt. Pomagamy firmom pokazać się online profesjonalnie, nowocześnie i skutecznie. Niezależnie, czy prowadzisz hotel, biuro nieruchomości czy firmę usługową, profesjonalna strona internetowa i immersyjne wirtualne spacery, pomogą Ci zdobyć zaufanie klientów i zwiększyć sprzedaż.",
+    button: {
+        label: "Oferta",
+        href: "#offer",
+    },
+    image: "",
+}
