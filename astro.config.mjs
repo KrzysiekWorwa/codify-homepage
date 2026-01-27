366e969e7c37ef4b70b84eca1a077da5456d6e8b
Krzysztof Worwa <worwak1998@gmail.com>
2026-01-27 14:16:28 +0100

    add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://krzysiekworwa.github.io',
+  base: "/codify-homepage",
   vite: {
     plugins: [tailwindcss()]
   }
