14a5a6fcb10de22373a2c2eda4bdd0f7ad6f97e3
Krzysztof Worwa <worwak1998@gmail.com>
2026-01-28 15:33:57 +0100

    feat: add hover background image effect to service cards

--- src/content/services.ts
+++ src/content/services.ts
@@ -1,6 +1,9 @@
 import WebIcon from "../assets/icons/web-design.svg";
 import VirtualIcon from "../assets/icons/360.svg";
 import GraphicIcon from "../assets/icons/graphic-icon.svg";
+import WebImage from "../assets/images/background/web-background.jpg";
+import VirtualImage from "../assets/images/background/360-background.jpg";
+import GraphicImage from "../assets/images/background/3d-background.jpg";
 
 export const SERVICES = [
     {
@@ -8,17 +11,23 @@ export const SERVICES = [
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non modi rerum exercitationem dolor tempora illum quis cum dolorum saepe reiciendis.",
         icon: WebIcon,
+        bgImage: WebImage,
+        href: "#web",
     },
     {
         title: "Wirtualne spacery 360°",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non modi rerum exercitationem dolor tempora illum quis cum dolorum saepe reiciendis.",
         icon: VirtualIcon,
+        bgImage: VirtualImage,
+        href: "#tour",
     },
     {
         title: "Grafika 3D",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non modi rerum exercitationem dolor tempora illum quis cum dolorum saepe reiciendis.",
         icon: GraphicIcon,
+        bgImage: GraphicImage,
+        href: "#graphic",
     },
 ];
