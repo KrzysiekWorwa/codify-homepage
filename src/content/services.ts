b4aab02b776170d7ff324e452af8d0e826024555
Krzysztof Worwa <worwak1998@gmail.com>
2026-01-28 14:31:00 +0100

    feat: add service icons and refactor content to data-driven structure

---
+++ src/content/services.ts
@@ -0,0 +1,24 @@
+import WebIcon from "../assets/icons/web-design.svg";
+import VirtualIcon from "../assets/icons/360.svg";
+import GraphicIcon from "../assets/icons/graphic-icon.svg";
+
+export const SERVICES = [
+    {
+        title: "Tworzenie stron internetowych",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non modi rerum exercitationem dolor tempora illum quis cum dolorum saepe reiciendis.",
+        icon: WebIcon,
+    },
+    {
+        title: "Wirtualne spacery 360°",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non modi rerum exercitationem dolor tempora illum quis cum dolorum saepe reiciendis.",
+        icon: VirtualIcon,
+    },
+    {
+        title: "Grafika 3D",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non modi rerum exercitationem dolor tempora illum quis cum dolorum saepe reiciendis.",
+        icon: GraphicIcon,
+    },
+];
