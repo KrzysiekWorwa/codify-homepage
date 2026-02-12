e440daa3268fe8292d4c5b400e2b719f373c1cb0
Krzysztof Worwa <worwak1998@gmail.com>
2026-02-12 16:46:10 +0100

    feat: update colors and adjust about section layout and buttons design

--- src/content/about.ts
+++ src/content/about.ts
@@ -1,7 +1,7 @@
 export const ABOUT = {
     id: "about",
-    header: "Zaufany Partner w rozwoju Twojej marki",
-    subHeader: "O nas",
+    header: "O nas",
+    subHeader: "Zaufany Partner w rozwoju Twojej marki",
     description: "Specjalizujemy się w cyfrowych rozwiązaniach, które przyciągają uwagę klientów i budują zaufanie do Twojej marki. Działamy kompleksowo - od pomysłu po gotowy projekt. Pomagamy firmom pokazać się online profesjonalnie, nowocześnie i skutecznie. Niezależnie, czy prowadzisz hotel, biuro nieruchomości czy firmę usługową, profesjonalna strona internetowa i immersyjne wirtualne spacery, pomogą Ci zdobyć zaufanie klientów i zwiększyć sprzedaż.",
     button: {
         label: "Oferta",
