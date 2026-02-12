a9df5d019c92f6e78451b941bb554b6d1f7f4c61
Krzysztof Worwa <worwak1998@gmail.com>
2026-02-12 20:12:11 +0100

    feat: add CTASection component

---
+++ src/content/cta.ts
@@ -0,0 +1,9 @@
+export const CTA = {
+    id: "cta",
+    header: "Wyróżnij się tam, gdzie liczy się pierwsze wrażenie",
+    description: "Porozmawiajmy o Twoim projekcie. Pokażemy Ci, jak nowoczesny i immersyjny design pomaga wyróżnić markę w sieci i przyciągnąć uwagę klientów.",
+    button: {
+        href: "#contact",
+        label: "Start",
+    },
+}
