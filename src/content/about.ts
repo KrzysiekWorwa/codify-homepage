ccfcfefd32a7e4d8720a7f73eb3118fbfd710d71
Krzysztof Worwa <worwak1998@gmail.com>
2026-02-12 19:12:19 +0100

    style: update gradient colors, refine layout and optimize image in About section

--- src/content/about.ts
+++ src/content/about.ts
@@ -4,8 +4,8 @@ export const ABOUT = {
     subHeader: "Zaufany Partner w rozwoju Twojej marki",
     description: "Specjalizujemy się w cyfrowych rozwiązaniach, które przyciągają uwagę klientów i budują zaufanie do Twojej marki. Działamy kompleksowo - od pomysłu po gotowy projekt. Pomagamy firmom pokazać się online profesjonalnie, nowocześnie i skutecznie. Niezależnie, czy prowadzisz hotel, biuro nieruchomości czy firmę usługową, profesjonalna strona internetowa i immersyjne wirtualne spacery, pomogą Ci zdobyć zaufanie klientów i zwiększyć sprzedaż.",
     button: {
-        label: "Oferta",
-        href: "#offer",
+        label: "Kontakt",
+        href: "#contact",
     },
     image: "",
 }
