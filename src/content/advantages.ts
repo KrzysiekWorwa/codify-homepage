657119c55086e20c97b2c4b6ed45b0655a5aac1e
Krzysztof Worwa <worwak1998@gmail.com>
2026-02-19 15:42:48 +0100

    feat: add and integrate SVG icons

--- src/content/advantages.ts
+++ src/content/advantages.ts
@@ -1,8 +1,13 @@
-import WebIcon from "../assets/icons/web-design.svg";
-import VirtualIcon from "../assets/icons/360.svg";
-import GraphicIcon from "../assets/icons/graphic-icon.svg";
+import StarIcon from "../assets/icons/tour/star.svg";
+import ShieldIcon from "../assets/icons/tour/shield_with_heart.svg";
+import DoorIcon from "../assets/icons/tour/door_open.svg";
+import HeartIcon from "../assets/icons/tour/heart_plus.svg";
+import ChartIcon from "../assets/icons/tour/chart_data.svg";
+import ClockIcon from "../assets/icons/tour/clock_arrow_up.svg";
+import SearchIcon from "../assets/icons/tour/person_search.svg";
+import RocketIcon from "../assets/icons/tour/rocket_launch.svg";
 
-type IconComponent = typeof WebIcon;
+type IconComponent = typeof StarIcon;
 
 export interface AdvantageItem {
     title: string;
@@ -28,49 +33,49 @@ export const ADVANTAGES_TOUR: AdvantagesData = {
             title: "Wyróżniasz się spośród konkurencji",
             description:
                 "Większość ofert nadal ogranicza się do zwykłych zdjęć, więc Twoja prezentacja od razu przyciągnie uwagę klientów.",
-            icon: WebIcon,
+            icon: StarIcon,
         },
         {
             title: "Budujesz zaufanie i profesjonalny wizerunek",
             description:
                 "Pokazując nieruchomość w pełni, dasz klientowi pewność co do jakości i wyglądu miejsca.",
-            icon: VirtualIcon,
+            icon: ShieldIcon,
         },
         {
             title: "Klienci „wchodzą” do Twojej przestrzeni",
             description:
                 "Dzięki pełnemu doświadczeniu 360°, można swobodnie zwiedzić obiekt z dowolnego miejsca i o dowolnej porze.",
-            icon: GraphicIcon,
+            icon: DoorIcon,
         },
         {
             title: "Pomagasz klientowi „poczuć” klimat miejsca",
             description:
                 "Wirtualny spacer pozwala lepiej wyobrazić sobie układ, wielkość i charakter apartamentu, co zwiększa szansę na rezerwację.",
-            icon: GraphicIcon,
+            icon: HeartIcon,
         },
         {
             title: "Więcej zapytań i rezerwacji",
             description:
                 "Badania pokazują, że ogłoszenia z wirtualnym spacerem generują większe zaangażowanie i dłuższy czas oglądania oferty.",
-            icon: WebIcon,
+            icon: ChartIcon,
         },
         {
             title: "Oszczędzasz czas",
             description:
                 "Wielu klientów po obejrzeniu spaceru dokładnie wie, czego się spodziewać, więc rezerwacje są bardziej świadome i pewne.",
-            icon: VirtualIcon,
+            icon: ClockIcon,
         },
         {
             title: "Lepsza widoczność online",
             description:
                 "Google promuje ogłoszenia z elementami 360°, co może zwiększyć pozycję Twojej strony lub oferty w wynikach wyszukiwania.",
-            icon: GraphicIcon,
+            icon: SearchIcon,
         },
         {
             title: "Idealne narzędzie promocyjne",
             description:
                 "Możesz wykorzystać spacer na stronie, w mediach społecznościowych lub Google Street View.",
-            icon: GraphicIcon,
+            icon: RocketIcon,
         },
     ],
 };
